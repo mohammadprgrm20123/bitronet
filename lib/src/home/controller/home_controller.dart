7b6b4178ad2491279b2886276a3541948ac71fc1
Mohammad Kazeminejad <Kazeminejad@yahoo.com>
2024-10-13 22:50:07 +0330

    complete home controller

--- lib/src/home/controller/home_controller.dart
+++ lib/src/home/controller/home_controller.dart
@@ -1,9 +1,26 @@
-
-
 import 'package:get/get.dart';
 
+import '../repositry/home_repository.dart';
+
 class HomeController extends GetxController {
+  final HomeRepository _repository = HomeRepository();
 
+  @override
+  void onInit() {
+    super.onInit();
+    getWorkPlaces(showActive: false);
+    getUserDetails();
+  }
 
+  Future<void> getWorkPlaces({required final bool showActive}) async {
+    final result = await _repository.getAllWorkPlace(showActive);
 
+    result.fold((final e) {}, (final r) {});
+  }
+
+  Future<void> getUserDetails() async {
+    final result = await _repository.getUserDetails();
+
+    result.fold((final e) {}, (final r) {});
+  }
 }
